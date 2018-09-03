d0f9f251de5a076dcd7856beff2917ef89e6dd66
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-09-03 17:36:02 +0900

    Add mainvisual slideshow

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
-import Header from './components/header_footer/Header'
 import './style.css';
+import Header from './components/header_footer/Header'
+import Featured from './components/featured'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <Header />
+       <Featured />
       </div>
     );
   }
