4fde88f80699b9aaddb5d5deb507b9dd91b84e2c
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-09-03 18:14:00 +0900

    Add TimeUntile component

---
+++ src/components/featured/TimeUntil.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Slide from 'react-reveal/Slide'
+
+export default class TimeUntil extends Component {
+
+  state = {
+
+  }
+
+  render() {
+    return <Slide left delay={1000}>
+        <div className="countdown_wrapper">
+          <div className="countdown_top">Event Starts in</div>
+          <div className="countdown_bottom">
+            <div className="countdown_item">
+              <div className="countdown_time">23</div>
+              <div className="countdown_tag">Days</div>
+            </div>
+            <div className="countdown_item">
+              <div className="countdown_time">23</div>
+              <div className="countdown_tag">Hs</div>
+            </div>
+            <div className="countdown_item">
+              <div className="countdown_time">23</div>
+              <div className="countdown_tag">Min</div>
+            </div>
+            <div className="countdown_item">
+              <div className="countdown_time">23</div>
+              <div className="countdown_tag">Sec</div>
+            </div>
+          </div>
+        </div>
+      </Slide>;
+  }
+}
