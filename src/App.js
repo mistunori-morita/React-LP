97da08ffcac2f45d56b089e201a5c22ee065d698
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-09-04 11:54:15 +0900

    Add Scroll Link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
 import './style.css';
+import {Element} from 'react-scroll'
+
 import Header from './components/header_footer/Header'
 import Featured from './components/featured'
 import VuneNfo from './components/VuneNfo'
 import Highlights from './components/Highlights'
 import Prcing from './components/Pricing'
 import Location from './components/location'
+import Footer from './components/header_footer/Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <Header />
+        <Element name="featured">
        <Featured />
+       </Element>
+        <Element name="venuenfo">
        <VuneNfo />
-       <Highlights />
+       </Element>
+        <Element name="highlihts">
+          <Highlights />
+       </Element>
+        <Element name="pricing">
        <Prcing />
-       <Location/>
+       </Element>
+        <Element name="location">
+        <Location/>
+       </Element>
+       <Footer/>
       </div>
     );
   }
--- src/components/header_footer/SideDrawer.js
+++ src/components/header_footer/SideDrawer.js
@@ -2,23 +2,34 @@ import React from 'react'
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import { scroller } from 'react-scroll'
 
 const SideDrawer = (props) => {
+
+  const scrollToElement = (el) => {
+    scroller.scrollTo(el, {
+      duraton: 1500,
+      delay:100,
+      smooth: true,
+      offset: -50
+    });
+    props.onClose(false)
+  }
   return <Drawer anchor="right" open={props.open} onClose={() => props.onClose(false)}>
       <List component="nav">
-        <ListItem button onClick={() => console.log("Event starts in")}>
+      <ListItem button onClick={() => scrollToElement('featured')}>
           Event starts in
         </ListItem>
-        <ListItem button onClick={() => console.log("Venue NFO")}>
+      <ListItem button onClick={() => scrollToElement('venuenfo')}>
           Venue NFO
         </ListItem>
-        <ListItem button onClick={() => console.log("Highlights")}>
+        <ListItem button onClick={() => scrollToElement('highlihts')}>
           Highlights
         </ListItem>
-        <ListItem button onClick={() => console.log("Pricing")}>
+      <ListItem button onClick={() => scrollToElement('pricing')}>
           Pricing
         </ListItem>
-        <ListItem button onClick={() => console.log("Location")}>
+      <ListItem button onClick={() => scrollToElement('location')}>
           Location
         </ListItem>
       </List>
